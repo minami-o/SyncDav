983edd454ef075f28ec28e25c56066ed5681f5fa
tuxgasy <contact@tuxgasy.fr>
2014-12-08 15:55:14 +0100

    Use Building Blocks

--- js/app.js
+++ js/app.js
@@ -1,43 +1,63 @@
-/* global DAVLib */
-
 'use strict';
 
 var App = function App() {
 
-  var store = null;
-  var info = null;
-  var saveConfigButton, fillButton, resetButton;
+  var btnSync, btnConfig, btnReset, btnResetConfirmed, btnCloseConfig, btnSaveConfig;
 
   var init = function init() {
-    info = document.getElementById('info');
-    saveConfigButton = document.getElementById('saveConfig');
-    fillButton = document.getElementById('fillDS');
-    resetButton = document.getElementById('resetDS');
+    btnSync = document.getElementById('sync');
+    btnConfig = document.getElementById('config');
+    btnReset = document.getElementById('reset');
+    btnResetConfirmed = document.getElementById('resetConfirmed');
+    btnCloseConfig = document.getElementById('closeConfig');
+    btnSaveConfig = document.getElementById('saveConfig');
 
-    if(localStorage.getItem('url') != null) {
-      document.getElementById('url').value = localStorage.getItem('url');
-      document.getElementById('user').value = localStorage.getItem('user');
-      document.getElementById('passwd').value = localStorage.getItem('passwd');
+    btnSync.addEventListener('click', handleEvent);
+    btnConfig.addEventListener('click', handleEvent);
+    btnReset.addEventListener('click', handleEvent);
+    btnResetConfirmed.addEventListener('click', handleEvent);
+    btnCloseConfig.addEventListener('click', handleEvent);
+    btnSaveConfig.addEventListener('click', handleEvent);
+    
+    if(localStorage.getItem('url') === null) {
+      document.getElementById('main').style.display = 'none';
+      document.getElementById('settings').style.display = 'block';
     }
-
-    saveConfigButton.addEventListener('click', handleEvent);
-    fillButton.addEventListener('click', handleEvent);
-    resetButton.addEventListener('click', handleEvent);
   };
 
   function handleEvent(evt) {
     var btn = evt.target.id;
 
     switch (btn) {
+      case 'config':
+        if(localStorage.getItem('url') != null) {
+          document.getElementById('url').value = localStorage.getItem('url');
+          document.getElementById('user').value = localStorage.getItem('user');
+          document.getElementById('passwd').value = localStorage.getItem('passwd');
+        }
+        document.getElementById('main').style.display = 'none';
+        document.getElementById('settings').style.display = 'block';
+        break;
+      case 'closeConfig':
+        document.getElementById('main').style.display = 'block';
+        document.getElementById('settings').style.display = 'none';
+        break;
       case 'saveConfig':
         localStorage.setItem('url', document.getElementById('url').value);
         localStorage.setItem('user', document.getElementById('user').value);
         localStorage.setItem('passwd', document.getElementById('passwd').value);
         
-        alert('Config saved');
+        document.getElementById('main').style.display = 'block';
+        document.getElementById('settings').style.display = 'none';
         break;
-      case 'fillDS':
-        fillButton.disabled = true;
+      case 'sync':
+        if(localStorage.getItem('url') === null) {
+          document.getElementById('main').style.display = 'none';
+          document.getElementById('settings').style.display = 'block';
+          return;
+        }
+        
+        btnSync.disabled = true;
 
         var accountData = {
           url: localStorage.getItem('url'),
@@ -60,6 +80,10 @@ var App = function App() {
             var contactsList = openedDAVMainResource.get().data;
             var nbContactsSaved = 0;
             var nbContactsNoSaved = 0;
+            
+            document.getElementById('syncStatus').style.display = 'block';
+            document.getElementById('nbContacts').textContent = contactsList.length;
+            
             for (var i = 0; i < contactsList.length; i++) {
               openedDAVConnection.getResource(contactsList[i].href, function(res, e) {
                 var vcard = res.contents;
@@ -128,17 +152,22 @@ var App = function App() {
                   saving.onsuccess = function() {
                     console.log('New contact saved');
                     nbContactsSaved++;
+                    document.getElementById('nbImported').textContent = nbContactsSaved;
                     if(nbContactsSaved + nbContactsNoSaved === contactsList.length) {
-                      alert(nbContactsSaved + ' / ' + contactsList.length + ' contact(s) saved');
-                      fillButton.disabled = false;
+                      btnSync.disabled = false;
+                      setTimeout(function() {
+                        document.getElementById('syncStatus').style.display = 'none';
+                      }, 5000);
                     }
                   };
                   saving.onerror = function(err) {
                     console.error(err);
                     nbContactsNoSaved++;
                     if(nbContactsSaved + nbContactsNoSaved === contactsList.length) {
-                      alert(nbContactsSaved + ' / ' + contactsList.length + ' contact(s) saved');
-                      fillButton.disabled = false;
+                      btnSync.disabled = false;
+                      setTimeout(function() {
+                        document.getElementById('syncStatus').style.display = 'none';
+                      }, 5000);
                     }
                   }
                 });
@@ -148,17 +177,29 @@ var App = function App() {
         };
         
         break;
-      case 'resetDS':
-        var q = confirm("Remove ALL the contacts from ? Be careful!!! There is no way back!");
-        if (q) {
-          var req = navigator.mozContacts.clear();
-          req.onsuccess = function() {
-            alert('All contacts have been removed.');
-          };
-          req.onerror = function() {
-            alert('[error] No contacts were removed.');
-          };
-        }
+      case 'reset':
+        document.getElementById('main').style.display = 'none';
+        document.getElementById('confirmDialog').style.display = 'block';
+        break;
+      case 'resetConfirmed':
+        document.getElementById('main').style.display = 'block';
+        document.getElementById('confirmDialog').style.display = 'none';
+        
+        btnReset.disabled = true;
+        
+        var req = navigator.mozContacts.clear();
+        req.onsuccess = function() {
+          document.getElementById('status').innerHTML = '<p>All contacts have been removed.</p>';
+          document.getElementById('status').style.display = 'block';
+          btnReset.disabled = false;
+          console.log('All contacts have been removed.');
+        };
+        req.onerror = function() {
+          document.getElementById('status').innerHTML = '<p>[error] No contacts were removed.</p>';
+          document.getElementById('status').style.display = 'block';
+          btnReset.disabled = false;
+          console.warn('No contacts were removed.');
+        };
         break;
       default:
         break;
